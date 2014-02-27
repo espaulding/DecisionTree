35f67d64791cf14a229432b375dcc5d83cc4f24b
Eric Spaulding <espaulding@users.noreply.github.com>
2014-02-26 20:56:53 -0700

    Update DecisionTreeLib.R
    fixed spacing strangeness (indentation) caused by sublime text

--- DecisionTreeLib.R
+++ DecisionTreeLib.R
@@ -7,31 +7,31 @@ set = list()
 #if a valid classLabelName is provided dataColumns and classLabelColumn will be superceded
 #classLabelName is case sensitive
 set$csvtoset = function(csvData, dataColumns=NULL, classLabelColumn=NULL, classLabelName=NULL){
-	newset = list()
-	if(!is.null(dataColumns)){ newset$data=csvData[,dataColumns]; }
-	if(!is.null(classLabelColumn)){ newset$class=csvData[,classLabelColumn]; }
+    newset = list()
+    if(!is.null(dataColumns)){ newset$data=csvData[,dataColumns]; }
+    if(!is.null(classLabelColumn)){ newset$class=csvData[,classLabelColumn]; }
 
-	classLabelColumn = which(colnames(csvData) == classLabelName)
-	if(length(classLabelColumn) == 1){ #length of 0 or more than 1 indicates an unuseable index
-		dataColumns = 1:ncol(csvData)
-		dataColumns = dataColumns[-classLabelColumn]
-		newset$data=csvData[,dataColumns]
-		newset$class=csvData[,classLabelColumn]
-	}
-	return(newset)
+    classLabelColumn = which(colnames(csvData) == classLabelName)
+    if(length(classLabelColumn) == 1){ #length of 0 or more than 1 indicates an unuseable index
+	dataColumns = 1:ncol(csvData)
+	dataColumns = dataColumns[-classLabelColumn]
+	newset$data=csvData[,dataColumns]
+	newset$class=csvData[,classLabelColumn]
+    }
+    return(newset)
 }
 
 #Given a set object remove the designated rows or columns from the set's data matrix and class vector
 set$subset = function(localset, rowsToRemove=NULL, colsToRemove=NULL){
-	ss = localset
-	if(!is.null(rowsToRemove)){ 
-		ss$data  = ss$data[-rowsToRemove,]
-		ss$class = ss$class[-rowsToRemove]
-	}
-	if(!is.null(colsToRemove)){ 
-		ss$data  = ss$data[,-colsToRemove]
-	}
-	return(ss)
+    ss = localset
+    if(!is.null(rowsToRemove)){ 
+	ss$data  = ss$data[-rowsToRemove,]
+	ss$class = ss$class[-rowsToRemove]
+    }
+    if(!is.null(colsToRemove)){ 
+	ss$data  = ss$data[,-colsToRemove]
+    }
+    return(ss)
 }
 
 #Use a decision tree previously built to classify a set of test data
@@ -51,13 +51,13 @@ set$subset = function(localset, rowsToRemove=NULL, colsToRemove=NULL){
 #            Unclassifiable samples are added to the number incorrectly classified
 #            as far as the accuracy calculations are concerned
 decision_tree$classify = function(set, tree, noanswer="vote"){
-	answers  = apply(set$data,1,function(r) decision_tree$classifyInstance(r,tree,noanswer=noanswer))
-	accuracy = NaN; if(!is.null(set$class)){ accuracy = set$class == answers; }
-	result   = list(answers=answers,accuracy=mean(accuracy))
-	result$numcorrect     = sum(accuracy)
-	result$numwrong       = length(accuracy) - result$numcorrect
-	result$unclassifiable = sum(answers == "unclassifiable")
-	return(result)
+    answers  = apply(set$data,1,function(r) decision_tree$classifyInstance(r,tree,noanswer=noanswer))
+    accuracy = NaN; if(!is.null(set$class)){ accuracy = set$class == answers; }
+    result   = list(answers=answers,accuracy=mean(accuracy))
+    result$numcorrect     = sum(accuracy)
+    result$numwrong       = length(accuracy) - result$numcorrect
+    result$unclassifiable = sum(answers == "unclassifiable")
+    return(result)
 }
 
 #Use a decision tree previously built to classify a single sample (test instance)
@@ -69,24 +69,24 @@ decision_tree$classifyInstance = function(sample, tree, noanswer="vote"){
     node = tree
     while(is.null(answer)){
     	if(node$type != "leaf"){
-    		value       = as.numeric(sample[node$dim])
-    		branchindex = which(node$decision == value)
-    		if(length(branchindex) == 1){ #follow the branch
-    			node = node$branch[[branchindex]]
-    		} else { #no branch to follow for this sample
-    			if(noanswer=="vote"){ answer = node$common; }
-    			else                { answer = "unclassifiable"; }
-    		}
-    	} else { #we're at a leaf node so collect its answer
-    		answer = node$answer
-    	}
+            value       = as.numeric(sample[node$dim])
+            branchindex = which(node$decision == value)
+            if(length(branchindex) == 1){ #follow the branch
+                node = node$branch[[branchindex]]
+            } else { #no branch to follow for this sample
+                if(noanswer=="vote"){ answer = node$common; }
+                    else                { answer = "unclassifiable"; }
+                }
+        } else { #we're at a leaf node so collect its answer
+            answer = node$answer
+        }
     }
     return(answer)
 }
 
 #Wrapper for the recursive ID3 function
 decision_tree$buildTree = function(train){
-	return(decision_tree$ID3(train, 1:ncol(train$data)))
+    return(decision_tree$ID3(train, 1:ncol(train$data)))
 }
 
 #Iterative Dichotomizer 3 algorithm
@@ -107,22 +107,22 @@ decision_tree$buildTree = function(train){
 #			Else
 #				Add subtree to this branch: ID3(Sv, attributes – this attribute)
 decision_tree$ID3 = function(nodedata, dims){
-	root = list(type="leaf",label="class")
-	counts = as.matrix(table(nodedata$class)) #count the number of set items from each class
-	root$common = rownames(counts)[which(counts == max(counts))][1]
+    root = list(type="leaf",label="class")
+    counts = as.matrix(table(nodedata$class)) #count the number of set items from each class
+    root$common = rownames(counts)[which(counts == max(counts))][1]
 
-	#base cases
-	classes = levels(as.factor(nodedata$class))
-	if(length(classes)==1 || length(dims)==0){ 
-		root$answer = root$common
-		return(root); 
-	}
+    #base cases
+    classes = levels(as.factor(nodedata$class))
+    if(length(classes)==1 || length(dims)==0){ 
+	root$answer = root$common
+	return(root); 
+    }
 
-	#main algorithm
-	root$type="node"
-	#find the dimension(attribute) with greatest information gain
-	infogainByDim   = sapply(c(1:length(dims)), function(d) decision_tree$infogain(nodedata,d))
-	indexBestDim    = order(infogainByDim, decreasing=TRUE)[1]
+    #main algorithm
+    root$type="node"
+    #find the dimension(attribute) with greatest information gain
+    infogainByDim   = sapply(c(1:length(dims)), function(d) decision_tree$infogain(nodedata,d))
+    indexBestDim    = order(infogainByDim, decreasing=TRUE)[1]
     root$label      = colnames(nodedata$data)[indexBestDim]
     root$dim        = dims[indexBestDim]
 
@@ -131,8 +131,8 @@ decision_tree$ID3 = function(nodedata, dims){
     datarows        = 1:nrow(nodedata$data)
     subsetRows      = sapply(root$decision, function(v) which(v==nodedata$data[,indexBestDim]))
 	
-	#generate a new node(subtree or leaf) to attach for each branch
-	root$branch = list()
+    #generate a new node(subtree or leaf) to attach for each branch
+    root$branch = list()
     for(v in 1:length(root$decision)){
     	subset = set$subset(nodedata, rowsToRemove=datarows[-subsetRows[[v]]], colsToRemove=indexBestDim)
     	if(nrow(subset$data) != 0){ root$branch[[v]] = decision_tree$ID3(subset,dims[-indexBestDim]); }
@@ -145,9 +145,10 @@ decision_tree$ID3 = function(nodedata, dims){
 #all 1 class in the set leads to 0 entropy (smallest entropy)
 #even distribution between multiple classes leads to 1 entropy (maximum entropy)
 decision_tree$entropy = function(c){
-	entropybyclass = function(p){ if(p == 0){ return(0) } 
-		                          return(-p*log2(p)) 
-		                        }
+    entropybyclass = function(p){ 
+        if(p == 0){ return(0) } 
+	return(-p*log2(p)) 
+    }
     e = sapply(levels(as.factor(c)), function(x) entropybyclass(length(which(c == x)) / length(c)))
     return(sum(e))
 }
@@ -166,14 +167,14 @@ decision_tree$entropy = function(c){
 #   data  : this must be a matrix of data with samples as rows and dimensions(attributes) as columns
 #   class : this must be a vector with 1 entry for each row of the matrix denoting that sample's class
 decision_tree$infogain = function(nodedata,dim){
-	weight_entropy = function(nd,sr){ 
-		weight  = length(nd$class[sr])/nrow(nd$data)
-		entropy = decision_tree$entropy(nd$class[sr])
+    weight_entropy = function(nd,sr){ 
+        weight  = length(nd$class[sr])/nrow(nd$data)
+        entropy = decision_tree$entropy(nd$class[sr])
         return(weight*entropy)
-	}
-	values        = levels(as.factor(nodedata$data[,dim])) #what are the discrete values for the dimension
+    }
+    values        = levels(as.factor(nodedata$data[,dim])) #what are the discrete values for the dimension
     subsetRows    = sapply(values, function(v) which(v==nodedata$data[,dim]))
     upper_entropy = decision_tree$entropy(nodedata$class)
     lower_entropy = sum(sapply(subsetRows, function(s) weight_entropy(nodedata,s)))
-	return(upper_entropy - lower_entropy)
+    return(upper_entropy - lower_entropy)
 }
